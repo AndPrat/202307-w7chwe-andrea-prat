13214e6f0395a4a562bc9423817db32d3c85af65
Andrea Prat <andreaprat@iMac-de-Andrea.local>
2023-08-27 11:17:51 +0200

    Create Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = (): React.ReactElement => {
+  return (
+    <header className="header">
+      <h1 className="header__title">Robots</h1>
+    </header>
+  );
+};
+
+export default Header;
