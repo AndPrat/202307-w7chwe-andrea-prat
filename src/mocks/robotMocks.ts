f7f9e8cae37abd5a15b73f7545a7e5fdc8ceeb94
Andrea Prat <andreaprat@iMac-de-Andrea.local>
2023-08-27 12:16:06 +0200

    Test CardComponent

--- src/mocks/robotMocks.ts
+++ src/mocks/robotMocks.ts
@@ -7,13 +7,22 @@ export const robotsMock: Robot[] = [
     image:
       "https://cosmocaixa.org/documents/139771/351893/WALL+E+OK+SIN+RECURSO+spa2AG+-+2422+-+18245jpg/c15e2c4e-2c8a-9216-ced1-fb5db4d38fbd?version=1.0&t=1568814933620",
     speed: 5,
-    endurance: 6,
+    endurace: 6,
   },
   {
     id: 2,
     name: "Eva",
     image: "https://www.spotern.com/es/product/67321?spotID=97980",
     speed: 8,
-    endurance: 7,
+    endurace: 7,
   },
 ];
+
+export const robotMock: Robot = {
+  id: 1,
+  name: "Wall-e",
+  image:
+    "https://cosmocaixa.org/documents/139771/351893/WALL+E+OK+SIN+RECURSO+spa2AG+-+2422+-+18245jpg/c15e2c4e-2c8a-9216-ced1-fb5db4d38fbd?version=1.0&t=1568814933620",
+  speed: 5,
+  endurace: 6,
+};
