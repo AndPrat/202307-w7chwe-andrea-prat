26c414262b513aa11f084403081d3195eac0d449
Andrea Prat <andr8ide@gmail.com>
2023-08-28 18:51:29 +0200

    Add NavigationBar styles with navigation, home and home.active classes

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./NavigationBar.css";
 
 const NavigationBar = (): React.ReactElement => {
   return (
     <nav className="navigation">
       <ul className="navigation__elements">
         <li>
-          <NavLink to="/robots" className="navigation__list">
-            Robots
+          <NavLink to="/robots" className="navigation__home">
+            Home
           </NavLink>
         </li>
       </ul>
