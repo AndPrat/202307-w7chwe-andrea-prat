0eb6bc834bd689143e9ab861d676a1a8f644eadf
Andrea Prat <andr8ide@gmail.com>
2023-08-28 22:41:53 +0200

    Create NewRobotPage page

---
+++ src/pages/NewRobotPage/NewRobotPage.tsx
@@ -0,0 +1,13 @@
+import "./NewRobotPage.css";
+
+const NewRobotPage = (): React.ReactElement => {
+  return (
+    <>
+      <div className="container">
+        <h2 className="robot__title">Create a robot</h2>
+      </div>
+    </>
+  );
+};
+
+export default NewRobotPage;
